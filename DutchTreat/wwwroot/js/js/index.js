3b3529ddf87f1318509e72502c85de301bdc34b5
Unknown <gdawson771@gmail.com>
2018-02-08 11:48:55 +0000

    Add project to repository

---
+++ DutchTreat/wwwroot/js/js/index.js
@@ -0,0 +1,13 @@
+﻿var x = 0;
+var s = "";
+
+console.log("Hello World");
+
+var form = document.getElementById("form");
+form.hidden = true;
+var button = document.getElementById("buyButton");
+
+button.on("click",
+    function() { console.log("Buying Item"); });
+var productInfo = document.getElementByClassName("product-props");
+var listItems=productInfo.item[0].children;
